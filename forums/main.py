9b594d68229b013913aa4218c254599c858870a3
zoheers <36574675+zoheers@users.noreply.github.com>
2018-03-26 16:51:56 +0300

    Add files via upload

---
+++ forums/main.py
@@ -0,0 +1,17 @@
+import models
+
+m1 = models.Member("zoheer",33)
+m2 = models.Member("ahmad" ,15)
+
+p1 = models.Post("python","python is programing langue")
+p2 = models.Post("python","python is programing langue")
+p3 = models.Post("python","python is programing langue")
+
+print m1.name
+print m1.age
+print m2.name
+print m2.age
+print p1.title
+print p1.topic
+
+
