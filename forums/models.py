fb8fa40b8f0f680172f2a64648d1c89e17a04e7b
zoheers <36574675+zoheers@users.noreply.github.com>
2018-03-26 16:52:20 +0300

    Update models.py

--- forums/models.py
+++ forums/models.py
@@ -1,10 +1,11 @@
-
 class Member():
 	def __init__(self,name,age):
 		self.name=name
 		self.age=age
 
 
-
-    
+class Post ():
+	def __init__(self,title,topic):
+		self.title=title
+		self.topic=topic
 		
