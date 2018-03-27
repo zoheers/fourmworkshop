7b50f30ac5bf4f77cb8671ecd55c13327996f9a8
zoheers <36574675+zoheers@users.noreply.github.com>
2018-03-27 15:36:26 +0300

    Update models.py

--- forums/models.py
+++ forums/models.py
@@ -1,11 +1,20 @@
 class Member():
+
 	def __init__(self,name,age):
 		self.name=name
 		self.age=age
 
-
+	def __str__(self):
+		return 'name : {}\t\tage:{}'.format(self.name,self.age)
+ 
 class Post ():
 	def __init__(self,title,topic):
 		self.title=title
 		self.topic=topic
+		
+	def __str__(self):
+		return 'Title : {}\t\n\nTopic:\n{}\n'.format(self.title , self.topic)
 
+
+			
+            
