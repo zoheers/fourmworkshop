5d7810dced021b648b1002d418444459e45811f1
zoheers <36574675+zoheers@users.noreply.github.com>
2018-03-27 15:35:35 +0300

    Update models.py

--- forums/models.py
+++ forums/models.py
@@ -9,20 +9,3 @@ class Post ():
 		self.title=title
 		self.topic=topic
 
-class MemberStore:
-	Members=[]
-	def add(self,s):
-		self.Members.append(s)
-	def get_all(self):
-		for p in self.Members:
-			print p.name , p.age
-
-
-
-class PostStore:
-	Posts=[]
-	def add(self,s):
-		self.Posts.append(s)
-	def get_all(self):
-		for p in self.Posts:
-			print p.title +'\n' +p.topic+'\n'
