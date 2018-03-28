370b1e2759fcfa2a6a9b18eb339538edaa589e88
zoheers <36574675+zoheers@users.noreply.github.com>
2018-03-28 18:34:19 +0300

    Update flask-demo.py

--- flask-demo/flask-demo.py
+++ flask-demo/flask-demo.py
@@ -1,1 +1,18 @@
+from flask import Flask
+from flask import Flask, render_template
 
+app = Flask(__name__)
+@app.route("/")
+@app.route("/index")
+def home():
+     return render_template("index.html")
+
+
+@app.route("/SayHello/YaserAlNajjar")
+def YaserAlNajjar():
+	
+	return "Hello YaserAlNajjar"
+
+
+
+app.run()
