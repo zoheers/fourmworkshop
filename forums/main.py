53f0213bb1639502440f5b38d2a4e8e27915fc56
zoheers <36574675+zoheers@users.noreply.github.com>
2018-03-28 14:19:57 +0300

    Update main.py
    test delete , entity_exists,get_by_id method

--- forums/main.py
+++ forums/main.py
@@ -1,4 +1,5 @@
 import models
+import store
 
 m1 = models.Member("zoheer",33)
 m2 = models.Member("ahmad" ,15)
@@ -7,13 +8,18 @@ p1 = models.Post("python","Python is an interpreted, object-oriented, high-level
 p2 = models.Post("html","Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.[4] Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.")
 p3 = models.Post("CSS","Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language.[1] Although most often used to set the visual style of web pages and user interfaces written in HTML and XHTML, the language can be applied to any XML document, including plain XML, SVG and XUL, and is applicable to rendering in speech, or on other media. Along with HTML and JavaScript, CSS is a cornerstone technology used by most websites to create visually engaging webpages, user interfaces for web applications, and user interfaces for many mobile applications")
 
-msotre = models.MemberStore()
+msotre = store.MemberStore()
 msotre.add(m1)
 msotre.add(m2)
+print msotre.get_by_id(2)
+print msotre.entity_exists(2)
+msotre.delete(2)
 msotre.get_all()
 
-pstore = models.PostStore()
+pstore = store.PostStore()
 pstore.add (p1)
 pstore.add (p2)
 pstore.add (p3)
+print pstore.entity_exists(1)
+print pstore.get_by_id(1)
 pstore.get_all()
